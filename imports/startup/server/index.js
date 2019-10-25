50b9685eb91df80122f3b75ee38f622b537f57d8
Doroshenko Maksym <Doroshenko.Maksym@flyuia.com>
2019-10-25 09:46:15 +0300

    make first schema and controller for apollo server

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,1 +1,25 @@
-console.log('Hi!');
+import { ApolloServer, gql } from 'apollo-server-express'
+import { WebApp } from 'meteor/webapp'
+import { getUser } from 'meteor/apollo'
+import merge from 'lodash/merge'
+
+// СХЕМЫ (Миграции)
+const typeDefs = `
+  type Query {
+    hi: String
+  }
+`;
+
+// КОНТРОЛЛЕРЫ
+const resolvers = {
+  Query: {
+    hi() {
+      return 'Hello World!'
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
